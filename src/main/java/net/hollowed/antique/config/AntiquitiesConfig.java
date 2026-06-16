f25796067e300b9d3f93b7572a4c9ff37b7b466a
The Typhothanian <thetyphothanian@gmail.com>
2026-06-16 17:13:28 -0600

    added default value to max cloth patterns

--- src/main/java/net/hollowed/antique/config/AntiquitiesConfig.java
+++ src/main/java/net/hollowed/antique/config/AntiquitiesConfig.java
@@ -4,5 +4,5 @@ import eu.midnightdust.lib.config.MidnightConfig;
 
 public class AntiquitiesConfig extends MidnightConfig {
     @Entry(name = "Max Cloth Patterns", min = 0, max = 16)
-    public static int MAX_CLOTH_PATTERNS;
+    public static int MAX_CLOTH_PATTERNS = 4;
 }
